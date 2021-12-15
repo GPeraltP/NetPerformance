0f72e9c2d318ebfb24e6f84972b067d02dee650b
Gerson <gerson.jp.lass@gmail.com>
2021-12-15 15:07:31 -0500

    add receiver

--- src/android/ServiceNet.java
+++ src/android/ServiceNet.java
@@ -168,6 +168,8 @@ public class ServiceNet extends Service {
         speedDownload.cancel(true);
         speedUpload.cancel(true);
         time.cancel();
+        Intent broadcastIntent = new Intent(this, SensorRestarterBroadcastReceiver.class);
+        sendBroadcast(broadcastIntent);
         if (mLocationManager != null) {
             for (int i = 0; i < mLocationListeners.length; i++) {
                 try {
